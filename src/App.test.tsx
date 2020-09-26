87abd1f8127d3f8c2c2516eae3c7a4c2e57ed0b3
Eric Horodyski <horodyski@ionic.io>
2020-09-26 18:43:57 -0400

    Lab: Mock UI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,53 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Plugins } from '@capacitor/core';
+import { isPlatform } from '@ionic/react';
+jest.mock('@ionic/react', () => {
+  const actual = jest.requireActual('@ionic/react');
+  return { ...actual, isPlatform: jest.fn() };
+});
 import App from './App';
 
-test('renders without crashing', () => {
-  const { baseElement } = render(<App />);
-  expect(baseElement).toBeDefined();
+describe('<App />', () => {
+  beforeEach(() => {
+    (Plugins.SplashScreen as any) = jest.fn();
+    (Plugins.SplashScreen.hide as any) = jest.fn();
+  });
+
+  describe('initialization', () => {
+    describe('in an Android context', () => {
+      beforeEach(() => (isPlatform as any).mockImplementation(() => true));
+      it('should hide the splash screen', () => {
+        const { container } = render(<App />);
+        expect(container).toBeDefined();
+        expect(Plugins.SplashScreen.hide).toHaveBeenCalledTimes(1);
+      });
+      afterEach(() => (isPlatform as any).mockRestore());
+    });
+
+    describe('in an Android context', () => {
+      beforeEach(() => (isPlatform as any).mockImplementation(() => true));
+      it('should hide the splash screen', () => {
+        const { container } = render(<App />);
+        expect(container).toBeDefined();
+        expect(Plugins.SplashScreen.hide).toHaveBeenCalledTimes(1);
+      });
+      afterEach(() => (isPlatform as any).mockRestore());
+    });
+
+    describe('in a web context', () => {
+      beforeEach(() => (isPlatform as any).mockImplementation(() => false));
+      it('should not hide the splash screen', () => {
+        const { container } = render(<App />);
+        expect(container).toBeDefined();
+        expect(Plugins.SplashScreen.hide).not.toHaveBeenCalled();
+      });
+      afterEach(() => (isPlatform as any).mockRestore());
+    });
+  });
+
+  afterEach(() => {
+    (Plugins.SplashScreen as any).mockRestore();
+    (Plugins.SplashScreen.hide as any).mockRestore();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonApp, IonRouterOutlet, isPlatform } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import { Plugins } from '@capacitor/core';
+import TeaPage from './tea/TeaPage';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -23,15 +24,22 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    const { SplashScreen } = Plugins;
+    if (isPlatform('capacitor')) SplashScreen.hide();
+  }, []);
+
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route path="/tea" component={TeaPage} exact={true} />
+          <Route exact path="/" render={() => <Redirect to="/tea" />} />
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
